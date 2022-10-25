9e40a01caeaaaac6fd3160b469ca5300e3479610
Julio Ferrer <julioferrer147@gmail.com>
2022-10-24 23:03:16 -0500

    Refactorizacion

---
+++ README.md
@@ -0,0 +1,9 @@
+# Frontend Mentor - Tip calculator app solution
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
--- app.js
+++ app.js
@@ -1,114 +1,102 @@
-const btnReset = document.querySelector('.main__btn');
-const billCheck = document.querySelector('#bill');
-const numPeople = document.querySelector('#people');
+// Variables
+const btnReset = document.querySelector(".main__btn");
+
+const billCheck = document.querySelector("#bill");
+let totalBill = parseInt(billCheck.value);
+
+const numPeople = document.querySelector("#people");
+let numOfPeople = parseInt(numPeople.value);
+
+const labels = document.querySelectorAll(".main__amount");
+const custom = document.querySelector("#custom");
 const selectTip = document.querySelectorAll('[type="radio"]');
-const formReset = document.querySelector('.main__section');
-const custom    = document.querySelector('#custom');
-const totalAmount = document.querySelector('#Amount');
-const totalPerPerson = document.querySelector('#Total');
+let tipSelection = 15;
+
+const totalAmount = document.querySelector("#Amount");
+const totalPerPerson = document.querySelector("#Total");
 
 const mesajeError = numPeople.nextElementSibling.nextElementSibling;
 
-let totalBill = 0;
-let tip = 0;
-let numOfPeople  = 0;
-let tipCustom =0;
+// AddEventListenners
+document.addEventListener("DOMContentLoaded", btnDisabled);
+billCheck.addEventListener("input", billUpdate);
+numPeople.addEventListener("input", undateNumOfPeople);
+btnReset.addEventListener("click", resetForm);
+custom.addEventListener("input", customTips);
+totals();
 
-eventListeners();
+// Actulizacion del CSS en select tip
+labels.forEach((element) =>
+  element.addEventListener("click", () => {
+    labels.forEach((btn) => {
+      btn.classList.remove("main__amount--selected");
+    });
+    element.classList.add("main__amount--selected");
+  })
+);
 
-function eventListeners () {
-    document.addEventListener('DOMContentLoaded',btnDisabled);
-    billCheck.addEventListener('keyup', setBill);
-    numPeople.addEventListener('keyup', setNumOfPeople);
-    selectTip.forEach( listOfTips =>{
-    listOfTips.addEventListener('click', setTip);});
-    custom.addEventListener('keyup', custumTip);
-    btnReset.addEventListener('click', resetForm);
+selectTip.forEach((btn) => {
+  btn.addEventListener("click", (evt) => {
+    tipSelection = parseInt(evt.target.value.slice(0, -1));
+    totals();
+  });
+});
+
+function billUpdate() {
+  totalBill = parseFloat(billCheck.value);
+  totals();
+}
+
+function undateNumOfPeople() {
+  numOfPeople = parseInt(numPeople.value);
+  if (numOfPeople === 0 || isNaN(numOfPeople)) {
+    showError();
+  } else {
+    numPeople.classList.remove("error");
+    mesajeError.classList.remove("error");
+    totals();
+  }
+}
+
+function customTips(evt) {
+  tipSelection = parseFloat(evt.target.value);
+  if (tipSelection <= 0 || isNaN(tipSelection)) return;
+  totals();
+}
+
+function totals() {
+  const amountPerson = ((totalBill * tipSelection) / 100 / numOfPeople).toFixed(
+    2
+  );
+  const totalPerson = (
+    ((totalBill * tipSelection) / 100 + totalBill) /
+    numOfPeople
+  ).toFixed(2);
+
+  totalAmount.textContent = amountPerson;
+  totalPerPerson.textContent = totalPerson;
+
+  btnReset.disabled = false;
+  btnReset.classList.remove("active");
+}
+
+function resetForm() {
+  billCheck.value = 0;
+  custom.value = "custom"
+  totalBill = 0;
+  numPeople.value = 2;
+  numOfPeople = 2;
+  totalAmount.textContent = "0.0";
+  totalPerPerson.textContent = "0.0";
+  btnDisabled();
 }
 
 function btnDisabled() {
-   btnReset.classList.add('active');
-   btnReset.disabled = true;
+  btnReset.classList.add("active");
+  btnReset.disabled = true;
 }
 
-function setBill () {
-    if(isNaN(billCheck.value) || billCheck.value <= 0){
-    }else{
-        totalBill = parseFloat(billCheck.value);
-    }
+function showError() {
+  numPeople.classList.add("error");
+  mesajeError.classList.add("error");
 }
-
-function setNumOfPeople () {
-
-    numOfPeople = parseInt(numPeople.value);
-
-    if( numOfPeople === '' || isNaN(numOfPeople ) || numOfPeople <= 0 ){
-        showError();
-    }else{
-        if(showError) {
-            numPeople.classList.remove('error');
-            mesajeError.classList.remove('error');
-        }
-        showHtml();
-    }
-}
-
-function setTip (e) {
-    tip = parseInt(e.target.value);
-    console.log(e.target);
-}
-
-function custumTip (e) {
-    tipCustom = e.target.value;
-    
-    if( isNaN(tipCustom ) || tipCustom <= 0 ){
-        mesajeError.classList.add('error');
-    }else{
-        if(showError) {
-            numPeople.classList.remove('error');
-            mesajeError.classList.remove('error');
-        }
-        showHtml();
-    }
-
-}
-
-function showError () {
-    numPeople.classList.add('error');
-    mesajeError.classList.add('error');
-}
-
-
-function showHtml () {
-
-    if( tipCustom === 0 ) {
-        let amount = Math.round((totalBill * tip / 100)
-        / numOfPeople );
-        let perPerson = Math.round((amount + totalBill / numOfPeople));
-        
-        totalAmount.textContent = `${amount}`
-        totalPerPerson.textContent = `${perPerson}`
-    } else {
-        
-        amount = Math.round((totalBill * tipCustom / 100)
-        / numOfPeople );
-        perPerson = Math.round((amount + totalBill / numOfPeople));
-        
-        totalAmount.textContent = `${amount}`
-        totalPerPerson.textContent =`${perPerson}`
-    }
-    btnReset.disabled = false;
-    btnReset.classList.remove('active');
-        
-}
-
-function resetForm () {
-    totalAmount.textContent = '0.0'
-    totalPerPerson.textContent = '0.0'
-    formReset.reset();
-    btnDisabled();
-}
-
-
-
-
